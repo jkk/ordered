06f7be96e232f18601b960c646960107e5275b2c
Justin Kramer <jkkramer@gmail.com>
2012-11-01 13:40:50 -0400

    aot compile the right namespace; bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.jkkramer/ordered "1.3.4"
+(defproject com.jkkramer/ordered "1.3.5"
   :url "https://github.com/jkk/ordered"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order. Fork of flatland/ordered with AOT compilation and no data_readers.clj."
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :aot [ordered.map ordered.set]
+  :aot [com.jkkramer.ordered.map com.jkkramer.ordered.set]
   ;; Not required except for benchmarks.
   :profiles {:dev {:dependencies [[ordered-collections "0.4.2"]]}})
